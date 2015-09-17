3e49ba42afb389459680b24e988da91cb7a5f863
Athan Clark <athan.clark@gmail.com>
2015-09-17 13:09:05 -0600

    implemented `matches

--- src/Data/Trie/Pred/Types.hs
+++ src/Data/Trie/Pred/Types.hs
@@ -11,12 +11,13 @@ module Data.Trie.Pred.Types where
 import Prelude hiding (lookup)
 import Data.Trie.Class
 import qualified Data.Trie.Map as MT
-import qualified Data.Map as M
+import qualified Data.Map as Map
 import Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.List.NonEmpty as NE
 
 import Data.Functor.Syntax
 import Data.Monoid
+import Data.Maybe (fromMaybe)
 
 
 -- * Single Predicated Step
@@ -63,6 +64,7 @@ instance Eq s => Monoid (PredSteps c s a) where
   mempty = PredSteps []
   mappend = unionPred
 
+-- | @Last@-style instance
 unionPred :: Eq s => PredSteps c s a -> PredSteps c s a -> PredSteps c s a
 unionPred (PredSteps (xss@(PredStep i p mx xs):pxs)) (PredSteps (yss@(PredStep j q my ys):pys))
   | i == j = PredSteps $ yss : unPredSteps (unionPred (PredSteps pxs) (PredSteps pys))
@@ -95,3 +97,52 @@ empty = PredTrie MT.empty (PredSteps [])
 -- subtrie :: Ord s => NonEmpty s -> PredTrie s a -> PredTrie s a
 -- subtrie (t:|ts) (PredTrie (MapTrie (MapStep ls)) ps)
 --   | null ts = getFirst $ First (lookup ts ls)
+
+-- | Find the nearest parent node of the requested query, while returning
+-- the split of the string that was matched, and what wasn't.
+match :: Ord s => NonEmpty s -> PredTrie s a -> Maybe (NonEmpty s, a, [s])
+match (t:|ts) (PredTrie ls (PredSteps ps)) = getFirst $
+  First (goLit ls) <> foldMap (First . goPred) ps
+  where
+    goLit (MT.MapStep xs) = do
+      (mx,mxs) <- Map.lookup t xs
+      let mFoundHere = do x <- mx
+                          return (t:|[], x, [])
+      if null ts then mFoundHere
+      else getFirst $ First (do (pre,y,suff) <- match (NE.fromList ts) =<< mxs
+                                return (t:|NE.toList pre, y, suff))
+                   <> First mFoundHere
+
+    goPred (PredStep _ p mx xs) = do
+      r <- p t
+      let mFoundHere = do x <- mx <$~> r
+                          return (t:|[], x, [])
+      if null ts then mFoundHere
+      else getFirst $ First (do (pre,y,suff) <- match (NE.fromList ts) xs
+                                return (t:|NE.toList pre, y r, suff))
+                   <> First mFoundHere
+
+
+matches :: Ord s => NonEmpty s -> PredTrie s a -> [(NonEmpty s, a, [s])]
+matches (t:|ts) (PredTrie ls (PredSteps ps)) =
+  fromMaybe [] $ getFirst $ First (goLit ls) <> foldMap (First . goPred) ps
+  where
+    goLit (MT.MapStep xs) = do
+      (mx,mxs) <- Map.lookup t xs
+      let mFoundHere = do x <- mx
+                          return [(t:|[],x,ts)]
+          prependAncestry (pre,x,suff) = (t:| NE.toList pre,x,suff)
+      if null ts then mFoundHere
+      else do foundHere <- mFoundHere
+              let rs = fromMaybe [] $ matches (NE.fromList ts) <$> mxs
+              return $ foundHere ++ (prependAncestry <$> rs)
+
+    goPred (PredStep _ p mx xs) = do
+      r <- p t
+      let mFoundHere = do x <- mx <$~> r
+                          return [(t:|[],x,ts)]
+          prependAncestryAndApply (pre,x,suff) = (t:| NE.toList pre,x r,suff)
+      if null ts then mFoundHere
+      else do foundHere <- mFoundHere
+              let rs = matches (NE.fromList ts) xs
+              return $ foundHere ++ (prependAncestryAndApply <$> rs)
