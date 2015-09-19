8855f66647469096b442aa13a19313b6b2ff3dac
Athan Clark <athan.clark@gmail.com>
2015-09-18 19:35:17 -0600

    better benchmark

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -5,27 +5,29 @@
 module Main where
 
 
-import           Data.Trie.Pred.Unified
-import qualified Data.Trie.Pred.Unified as U
+import Prelude hiding (lookup)
+import           Data.Trie.Pred
+import           Data.Trie.Pred.Step (PredStep (..), PredSteps (..))
+import           Data.Trie.Class
+import           Data.Trie.Map (MapStep (..))
+import qualified Data.Map as Map
 import           Data.List.NonEmpty
 import qualified Data.List.NonEmpty as NE
 import qualified Data.Text as T
 import           Data.Attoparsec.Text
 import           Criterion.Main
+import           Data.Set.Class as Sets
 
 
-doubleLit :: RUPTrie T.Text Double
-doubleLit = Rooted Nothing
-  [ UMore "1" (Just 1) []
-  , UMore "2" (Just 2) []
-  , UMore "3" (Just 3) []
-  , UMore "4" (Just 4) []
-  ]
+doubleLit :: RootedPredTrie T.Text Double
+doubleLit = RootedPredTrie Nothing $ PredTrie
+              (MapStep $ unUnion $ foldMap (Union . genStep) [1..100])
+              (PredSteps [])
+  where genStep n = Map.singleton (T.pack $ show n) (Just n, Nothing)
 
-doubleAtto :: RUPTrie T.Text Double
-doubleAtto = Rooted Nothing
-  [ UPred "d" (eitherToMaybe . parseOnly double) (Just id) []
-  ]
+doubleAtto :: RootedPredTrie T.Text Double
+doubleAtto = RootedPredTrie Nothing $ PredTrie mempty $ PredSteps
+  [PredStep "d" (eitherToMaybe . parseOnly double) (Just id) mempty]
   where
     eitherToMaybe (Left _) = Nothing
     eitherToMaybe (Right a) = Just a
@@ -34,16 +36,18 @@ doubleAtto = Rooted Nothing
 main = defaultMain
   [ bgroup "Lit vs. Pred"
     [ bgroup "Lit"
-      [ bench "1" $ whnf (U.lookup ["1"]) doubleLit
-      , bench "2" $ whnf (U.lookup ["2"]) doubleLit
-      , bench "3" $ whnf (U.lookup ["3"]) doubleLit
-      , bench "4" $ whnf (U.lookup ["4"]) doubleLit
+      [ bench "1" $ whnf (lookup ["1"]) doubleLit
+      , bench "2" $ whnf (lookup ["21"]) doubleLit
+      , bench "3" $ whnf (lookup ["41"]) doubleLit
+      , bench "4" $ whnf (lookup ["61"]) doubleLit
+      , bench "4" $ whnf (lookup ["81"]) doubleLit
       ]
     , bgroup "Pred"
-      [ bench "1" $ whnf (U.lookup ["1"]) doubleAtto
-      , bench "2" $ whnf (U.lookup ["2"]) doubleAtto
-      , bench "3" $ whnf (U.lookup ["3"]) doubleAtto
-      , bench "4" $ whnf (U.lookup ["4"]) doubleAtto
+      [ bench "1" $ whnf (lookup ["1"]) doubleAtto
+      , bench "2" $ whnf (lookup ["21"]) doubleAtto
+      , bench "3" $ whnf (lookup ["41"]) doubleAtto
+      , bench "4" $ whnf (lookup ["61"]) doubleAtto
+      , bench "4" $ whnf (lookup ["81"]) doubleAtto
       ]
     ]
   ]
